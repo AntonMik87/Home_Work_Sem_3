23df4bed52e9b36a07aadb7a0607272b44e47565
«Anton» <hranitel-liks@mail.ru>
2022-08-17 01:17:48 +0700

    add HW004

---
+++ Tasksk/HW004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+Console.WriteLine("Введите число: " );
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[] array = new int[10];
+Console.WriteLine(" ");
+for (int i = 0; i < array.Length; i++)
+{
+
+    array[i] = new Random().Next(1, 10);
+    Console.WriteLine(array[i]);
+    if (array[i] == n)
+    {
+
+        Console.Write("Индекс совпавшего числа: " + i);
+        break;
+    }
+}
+
