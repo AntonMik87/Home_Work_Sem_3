619cbd2166a65a8cf3f6cd2038acb37fd7cdc125
«Anton» <hranitel-liks@mail.ru>
2022-08-17 03:14:45 +0700

    add HW005

---
+++ Tasksk/HW005/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. 
+// Показать количество нечетных\четных чисел
+
+int[] array = new int[8];
+int sumChet = 0;
+// int sumNech = 0;
+for (int i = 0; i < array.Length; i++)
+{
+
+    array[i] = new Random().Next(1, 8);
+    Console.WriteLine(array[i]);
+    if (array[i] % 2 == 0)
+    {
+
+        sumChet = array[i];
+    }
+
+}
+
+Console.WriteLine($"количество четных: {sumChet}");
