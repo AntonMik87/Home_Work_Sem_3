9f93400230bd19b294ec4157ca2acd4133f62afd
«Anton» <hranitel-liks@mail.ru>
2022-08-17 00:20:04 +0700

    add HW003

---
+++ Tasksk/HW003/Program.cs
@@ -0,0 +1,21 @@
+// Написать программу замену элементов массива на противоположные
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    Console.Write(array[i] + " ");
+
+    if (array[i] < 0)
+    {
+        array[i] = array[i] * -1;
+    }
+    else
+    {
+        array[i] = -array[i];
+    } 
+    Console.Write("  ");
+    Console.WriteLine(array[i]);
+   
+}
