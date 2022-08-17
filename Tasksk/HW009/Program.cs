1b2528ddb2f272c91f56b5de16c627cc069efc2c
«Anton» <hranitel-liks@mail.ru>
2022-08-18 02:37:14 +0700

    add HW009

---
+++ Tasksk/HW009/Program.cs
@@ -0,0 +1,16 @@
+﻿// // В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+double[] array = new double[10];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    
+    {
+        array[i] = random.NextDouble() * 100;
+        Console.Write("{0,6:F2}", array[i]);
+    }
+}   
+double max = 0;
+double min = 0;
+if(array [i]> max) max = array[i];
+
+Console.WriteLine("MAX" + max);
