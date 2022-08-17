8c52100286b3281c1189ff006aaa8d44ba489b25
Anton <hranitel-liks@mail.ru>
2022-08-17 16:21:09 +0700

    add HW006

--- Tasksk/HW006/Program.cs
+++ Tasksk/HW006/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int[] array = new int[9];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+
+    Console.Write($" {array[i]}");
+}
+for (int a = 0; a < array.Length; a++)
+{
+    if (array[a] >= 1 - array[a] <= 5)
+        sum = sum + 1;
+
+
+}
+Console.Write($" Сумма элементов отрезка [10, 99]: {sum}  ");
