fca8200a60552d7db30c1697f4a8ec69ce5fd45b
«Anton» <hranitel-liks@mail.ru>
2022-08-18 00:19:06 +0700

    add HW009

---
+++ Tasksk/HW008/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+int[] array = new int[20];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 20);
+    Console.Write($" {array[i]}");
+}
+
+
+Console.WriteLine(" ");
+
+
+int g = array.Length -1;
+for (int i = 0; i < (array.Length+1) / 2; i++)
+{
+    Console.Write($" | {array[i] * array[g]} ");
+    g--;
+}
