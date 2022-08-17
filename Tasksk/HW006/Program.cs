27865c53fe308e220db064ec7537f215faab8102
«Anton» <hranitel-liks@mail.ru>
2022-08-17 21:15:55 +0700

    add HW007

--- Tasksk/HW006/Program.cs
+++ Tasksk/HW006/Program.cs
@@ -1,16 +1,16 @@
 ﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
-int[] array = new int[9];
+int[] array = new int[123];
 int sum = 0;
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(1, 10);
+    array[i] = new Random().Next(1, 123);
 
     Console.Write($" {array[i]}");
 }
 for (int a = 0; a < array.Length; a++)
 {
-    if (array[a] >= 1 - array[a] <= 5)
+    if (array[a] >= 10 && array[a] <= 99)
         sum = sum + 1;
 
 
---
+++ Tasksk/HW007/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+//               0   1  2    3   4  5   6    7   8
+int[] array = { 11, 23, 34, 44, 56, 16, 71, 18, 19 };
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($" {array[i]}");
+}
+Console.Write(" ");
+int SumNechet = 0;
+for (int j = 1; j < array.Length; j++)
+{
+    if (j % 2 != 0) SumNechet = SumNechet + array[j];
+    
+}
+Console.Write($" Сумма нечетных элементов массива: {SumNechet}");
