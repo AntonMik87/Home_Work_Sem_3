3439a7f8583d1a8f7d1903367fb84940c1baa63b
«Anton» <hranitel-liks@mail.ru>
2022-08-18 19:52:04 +0700

    add HW009

--- Tasksk/HW009/Program.cs
+++ Tasksk/HW009/Program.cs
@@ -3,14 +3,21 @@ double[] array = new double[10];
 Random random = new Random();
 for (int i = 0; i < array.Length; i++)
 {
-    
-    {
-        array[i] = random.NextDouble() * 100;
-        Console.Write("{0,6:F2}", array[i]);
-    }
-}   
-double max = 0;
-double min = 0;
-if(array [i]> max) max = array[i];
 
-Console.WriteLine("MAX" + max);
+
+    array[i] = random.NextDouble() * 100;
+    Console.Write(" {0,6:F2} ", array[i]);
+
+}
+double max = array[0];
+double min = array[0];
+for (int g = 0; g < array.Length; g++)
+{
+    if (array[g] > max) max = array[g];
+    if (array[g] < min) min = array[g];
+}
+Console.Write(" MAX:" + "{0,6:F2}", max);
+Console.Write(" ");
+Console.Write(" MIN:" + "{0,6:F2}", min);
+Console.Write(" ");
+Console.Write(" MAX - MIN:" + "{0,6:F2}", max - min);
